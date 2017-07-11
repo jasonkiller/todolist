61472b3b6ded18edaf8ef1a2f7813887c0251e0a
houyingdong <yingdong.hou@taskwedo.com>
2017-07-11 13:44:41 +0800

    modify a

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,15 +7,15 @@
 <div>
    <h3>Contents</h3>
    <ul>
-      <a href="http://godsondaily:81"><li>81: todolist</li></a>
-      <a href="http://godsondaily:82"><li>82: godsondaily</li></a>
-      <a href="http://godsondaily:85"><li>85: october</li></a>
-      <a href="http://godsondaily:86"><li>86: microweber</li></a>
-      <a href="http://godsondaily:88"><li>88: antvel</li></a>
-      <a href="http://godsondaily:89"><li>89: persimmon</li></a>
-      <a href="http://godsondaily:90"><li>90: katana</li></a>
-      <a href="http://godsondaily:91"><li>91: notadd</li></a>
-      <a href="http://godsondaily:93"><li>93: laralite</li></a>
+      <a href="http://godsondaily.xyz:81"><li>81: todolist</li></a>
+      <a href="http://godsondaily.xyz:82"><li>82: godsondaily.xyz</li></a>
+      <a href="http://godsondaily.xyz:85"><li>85: october</li></a>
+      <a href="http://godsondaily.xyz:86"><li>86: microweber</li></a>
+      <a href="http://godsondaily.xyz:88"><li>88: antvel</li></a>
+      <a href="http://godsondaily.xyz:89"><li>89: persimmon</li></a>
+      <a href="http://godsondaily.xyz:90"><li>90: katana</li></a>
+      <a href="http://godsondaily.xyz:91"><li>91: notadd</li></a>
+      <a href="http://godsondaily.xyz:93"><li>93: laralite</li></a>
    </ul>
 </div>
 </body>
